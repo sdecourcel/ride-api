d15501e4c79da9c42c70100360177bba508f0064
Sibylle de Courcel <sibylle44@gmail.com>
2019-05-13 19:25:45 +0200

    add validations to ride model

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,6 +1,11 @@
 class Ride < ApplicationRecord
   enum status: { created: 0, started: 1, completed: 2, cancelled: 3 }
 
+  validates :token, presence: true
+  validates :token, uniqueness: true
+  validates :token, format: { with: /\A[a-zA-Z0-9]{4}\z/ }
+  validates :status, presence: true
+
   before_validation :generate_token, on: :create
 
   private
@@ -11,5 +16,5 @@ class Ride < ApplicationRecord
       break random_token unless self.class.exists?(token: random_token)
     end
   end
-  
+
 end
