8d44360ce7af7c9d3aed9b35f68ef119a086014b
Sibylle de Courcel <sibylle44@gmail.com>
2019-05-13 23:33:37 +0200

    add rides controller with four actions

---
+++ app/controllers/api/v1/rides_controller.rb
@@ -0,0 +1,62 @@
+class Api::V1::RidesController < Api::V1::BaseController
+  before_action :set_ride, except: :create
+
+  def create
+    @ride = Ride.new
+    if @ride.save
+      display_status
+      render json: @ride.as_json
+    else
+      render_error
+    end
+  end
+
+  def bill_and_update_status_to_started
+    if @ride.generate_bill
+      display_status
+      render json: @ride.as_json
+    else
+      render_error
+    end
+  end
+
+  def pay_and_update_status_to_completed
+    if @ride.proceed_payment
+      display_status
+      render json: @ride.as_json
+    else
+      render_error
+    end
+  end
+
+  def reimburse_and_update_status_to_cancelled
+    if @ride.proceed_reimburse
+      display_status
+      render json: @ride.as_json
+    else
+      render_error
+    end
+  end
+
+  private
+
+  def set_ride
+    @ride = Ride.find_by_token(params[:token])
+  end
+
+  def display_status
+    puts "Trajet #{@ride.token} : #{@ride.explicit_status}"
+  end
+
+  def render_error
+    error_messages = @ride.errors.messages
+    puts "Trajet #{@ride.token} :"
+    error_messages.each do |message|
+      puts message
+    end
+    render json: { errors: error_messages }, status: :unprocessable_entity
+  end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :rides, only: [ :create ]
-      patch "started", to: "rides#bill_and_update_status_to_started"
-      patch "completed", to: "rides#pay_and_update_status_to_completed"
-      patch "cancelled", to: "rides#reimburse_and_update_status_to_cancelled"
+      patch "start", to: "rides#bill_and_update_status_to_started"
+      patch "complete", to: "rides#pay_and_update_status_to_completed"
+      patch "cancel", to: "rides#reimburse_and_update_status_to_cancelled"
     end
   end
 
