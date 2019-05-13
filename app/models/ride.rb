24328ea0b0719fec231dfb3b7bd350d7ee9905b3
Sibylle de Courcel <sibylle44@gmail.com>
2019-05-13 19:21:16 +0200

    add generate_token method before validation on create to ride model

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,15 @@
 class Ride < ApplicationRecord
   enum status: { created: 0, started: 1, completed: 2, cancelled: 3 }
 
+  before_validation :generate_token, on: :create
+
+  private
+
+  def generate_token
+    self.token = loop do
+      random_token = SecureRandom.hex(2)
+      break random_token unless self.class.exists?(token: random_token)
+    end
+  end
+  
 end
