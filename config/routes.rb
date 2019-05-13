9247406bcd20880babbed428abc22a7889356686
Sibylle de Courcel <sibylle44@gmail.com>
2019-05-13 19:09:15 +0200

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :rides, only: [ :create ]
+      patch "started", to: "rides#bill_and_update_status_to_started"
+      patch "completed", to: "rides#pay_and_update_status_to_completed"
+      patch "cancelled", to: "rides#reimburse_and_update_status_to_cancelled"
+    end
+  end
+
 end
